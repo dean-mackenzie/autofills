0cc4ea842a06867442a959600253f6c602687a91
unknown <nathan_brennan@flightcentre.com>
2015-04-23 08:44:59 +1000

    - Set details for any combination of adult, child an infant passengers - Add auto scroll to bottom

--- Pax.Autofill.js
+++ Pax.Autofill.js
@@ -2,17 +2,18 @@ var passengerEmailAddress = 'USE_YOUR_EMAIL@ADDRESS.COM';
 
 // these $('#' + key) will be filled out with the value in the key/value pair
 var formValues = {
-  passengerForm_passengers_0_adultTitle: 'Mr',
-  passengerForm_passengers_0_firstName: 'Test',
-  passengerForm_passengers_0_surname: 'Test',
-  passengerForm_passengers_0_gender: 'Male',
-  passengerForm_passengers_0_dateOfBirth_day: '2',
-  passengerForm_passengers_0_dateOfBirth_month: '3',
-  passengerForm_passengers_0_dateOfBirth_year: '1977',
-  passengerForm_passengers_0_passport_passportNumber: '123456789',
-  passengerForm_passengers_0_passport_expiry_day: '1',
-  passengerForm_passengers_0_passport_expiry_month: '1',
-  passengerForm_passengers_0_passport_expiry_year: '2033',
+  _adultTitle: 'Mr',
+  _childInfantTitle: 'Miss',
+  _firstName: 'Test',
+  _surname: 'Test',
+  _gender: 'Male',
+  _dateOfBirth_day: '2',
+  _dateOfBirth_month: '3',
+  _dateOfBirth_year: '1977',
+  _passport_passportNumber: '123456789',
+  _passport_expiry_day: '1',
+  _passport_expiry_month: '1',
+  _passport_expiry_year: new Date().getFullYear() + 2,
   selectContactPerson: '0',
   passengerForm_contactDetails_address_addressLine1: '100 Test Street',
   passengerForm_contactDetails_address_suburb: 'Brisbane',
@@ -29,14 +30,45 @@ var clickMe = [
   'passengerForm_namesChecked'
 ];
 
-var elementID, inputValue, elementToClick;
-
-$.each(formValues, function(elementID, inputValue) {
-  $('#' + elementID).val(inputValue);
+// Sub in the defult values for all fields
+$.each(formValues, function(fieldSelector, inputValue) {
+  $('select[id*="' + fieldSelector + '"],input[id*="' + fieldSelector + '"]').val(inputValue);
 });
 
+// Give every passenger a unique name to avoid dupe passenger error
+var letters = 'abcdefghijkl';
+$.each($('input[id*="_firstName"]'), function(index, element) {
+  $(element).val('Test Passenger ' + letters[index]);
+});
+
+// Create dates which represent the DoB for child and infant passegers
+var childDob = new Date(new Date().setYear(new Date().getFullYear() - 10)),
+    infantDob = new Date(new Date().setYear(new Date().getFullYear() - 1));
+
+// Adjust DoB inputs for Child passangers
+var $childPassengerDetails = $('.passenger-identity input[value="CHILD"]').closest('.passenger-details');
+$.each($childPassengerDetails, function(index, element) {
+  $(element).find('select[id*="_dateOfBirth_day"]').val(childDob.getDate());
+  $(element).find('select[id*="_dateOfBirth_month"]').val(childDob.getMonth());
+  $(element).find('select[id*="_dateOfBirth_year"]').val(childDob.getFullYear());
+});
+
+// Adjust DoB inputs for Infant passangers
+var $childPassengerDetails = $('.passenger-identity input[value="INFANT"]').closest('.passenger-details');
+$.each($childPassengerDetails, function(index, element) {
+  $(element).find('select[id*="_dateOfBirth_day"]').val(infantDob.getDate());
+  $(element).find('select[id*="_dateOfBirth_month"]').val(infantDob.getMonth());
+  $(element).find('select[id*="_dateOfBirth_year"]').val(infantDob.getFullYear());
+});
+
+// Click on the elements that require clicking
 $.each(clickMe, function(count, elementToClick) {
   $('#' + elementToClick).click();
 });
 
 $('form :input:visible').change();
+
+// Scroll to the Continue button
+$('body').animate({
+  scrollTop: $("#passengerCont").offset().top
+}, 500);
